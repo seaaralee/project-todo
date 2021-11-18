186231a9ebf894b6146481d00367f0aafa1d3835
Mutia Meilani <87632337+seaaralee@users.noreply.github.com>
2021-11-19 02:15:07 +0700

    Add files via upload

---
+++ show-task.php
@@ -0,0 +1,27 @@
+<?php 
+
+include 'config.php';
+
+$sql = "SELECT * FROM tasks";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+
+?>
+
+<li>
+    <span class="text"><?php echo $row['title']; ?></span>
+    <div>
+        <i id="removeBtn" class="icon fa fa-trash" data-id="<?php echo $row['id']; ?>"></i>
+    </div>
+</li>
+
+<?php
+    }
+    echo '<div class="pending-text">You have ' . mysqli_num_rows($result) . ' pending tasks.</div>';
+} else {
+    echo "<li><span class='text'>No Record Found.</span></li>";
+}
+
+?>
